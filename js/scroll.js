feebabf2d1724935087be4819e7036fe89ea6626
ZijunNi <133508406+ZijunNi@users.noreply.github.com>
2025-03-05 15:38:15 +0800

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,58 +1,58 @@
-// const backgroundData = [
-//     {
-//         image: './assets/images/background/bg2.jpg',
-//         chinese_title: '抚远 · 东极广场',
-//         english_title: 'East Polar Plaza, Fuyuan, China',
-//         pic_date: '摄于2025年1月1日 Pictured at Jan. 1st, 2025'
-//     },
-//     {
-//         image: './assets/images/background/bg1.jpg',
-//         chinese_title: '富锦 · 001乡道',
-//         english_title: 'Village Road 001, Fujin, China',
-//         pic_date: '摄于2025年1月1日 Pictured at Jan. 1st, 2025'
-//     },
-//     {
-//         image: './assets/images/background/bg3.jpg',
-//         chinese_title: '富锦 · 锦山 <span style="font-size:0.5em">（别拉音子山）</span>',
-//         english_title: 'Jinshan Hill <span style="font-size:0.5em">(Bielayin Hill)</span>, Fujin, China',
-//         pic_date: '摄于2024年12月31日 Pictured at Dec. 31st, 2024'
-//     },
-//     {
-//         image: './assets/images/background/bg4.jpg',
-//         chinese_title: '延庆 · 野鸭湖国家湿地公园',
-//         english_title: 'Yeyahu National Wetland Park, Yanqing, China',
-//         pic_date: '摄于2024年12月3日 Pictured at Dec. 3rd, 2024'
-//     },
-//     {
-//         image: './assets/images/background/bg5.jpeg',
-//         chinese_title: '柏林 · 苏军纪念碑',
-//         english_title: 'Sowjetisches Ehrenmal, Berlin, Deutschland<br/>Soviet War Memorial, Berlin, Germany',
-//         pic_date: '摄于2024年11月16日 Pictured at Nov. 16th, 2024'
-//     },   
-//     {
-//         image: './assets/images/background/bg6.jpg',
-//         chinese_title: '柏林 · 奥伯鲍姆桥',
-//         english_title: '<span style="font-size:80%;">Oberbaumbrüke, Berlin, Deutschland<br/>Oberbaum Bridge, Berlin, Germany</span>',
-//         pic_date: '<span style="font-size:90%;">摄于2024年11月16日 Pictured at Nov. 16th, 2024</span>'
-//     },    
-    
-//     // 暗色图片专用样式
-//     {
-//         image: './assets/images/background/bg7.jpg',
-//         chinese_title: '<span style="color: var(--bs-gray-400);">南昌 · 赣江两岸</span>',
-//         english_title: '<span style="color: var(--bs-gray-400);">Two Sides of the Ganjiang river</span>',
-//         pic_date: '<span style="color: var(--bs-gray-400);">摄于2025年2月5日 Pictured at Feb. 5th, 2025</span>'
-//     },    
-// ];
-
 const backgroundData = [
     {
-        image: './assets/images/background/special_day.jpeg',
-        chinese_title: '<span style="color: var(--bs-gray-400);">你怎么知道我给我对象450抽出了9个金？</span>',
-        english_title: '<span style="color: var(--bs-gray-400);">How did you know I got 9 golds in 450 pulls for my GF?</span>',
-        pic_date: '<span style="color: var(--bs-gray-400);">于2025年3月4日  At Mar. 4th, 2025</span>'
+        image: './assets/images/background/bg2.jpg',
+        chinese_title: '抚远 · 东极广场',
+        english_title: 'East Polar Plaza, Fuyuan, China',
+        pic_date: '摄于2025年1月1日 Pictured at Jan. 1st, 2025'
     },
+    {
+        image: './assets/images/background/bg1.jpg',
+        chinese_title: '富锦 · 001乡道',
+        english_title: 'Village Road 001, Fujin, China',
+        pic_date: '摄于2025年1月1日 Pictured at Jan. 1st, 2025'
+    },
+    {
+        image: './assets/images/background/bg3.jpg',
+        chinese_title: '富锦 · 锦山 <span style="font-size:0.5em">（别拉音子山）</span>',
+        english_title: 'Jinshan Hill <span style="font-size:0.5em">(Bielayin Hill)</span>, Fujin, China',
+        pic_date: '摄于2024年12月31日 Pictured at Dec. 31st, 2024'
+    },
+    {
+        image: './assets/images/background/bg4.jpg',
+        chinese_title: '延庆 · 野鸭湖国家湿地公园',
+        english_title: 'Yeyahu National Wetland Park, Yanqing, China',
+        pic_date: '摄于2024年12月3日 Pictured at Dec. 3rd, 2024'
+    },
+    {
+        image: './assets/images/background/bg5.jpeg',
+        chinese_title: '柏林 · 苏军纪念碑',
+        english_title: 'Sowjetisches Ehrenmal, Berlin, Deutschland<br/>Soviet War Memorial, Berlin, Germany',
+        pic_date: '摄于2024年11月16日 Pictured at Nov. 16th, 2024'
+    },   
+    {
+        image: './assets/images/background/bg6.jpg',
+        chinese_title: '柏林 · 奥伯鲍姆桥',
+        english_title: '<span style="font-size:80%;">Oberbaumbrüke, Berlin, Deutschland<br/>Oberbaum Bridge, Berlin, Germany</span>',
+        pic_date: '<span style="font-size:90%;">摄于2024年11月16日 Pictured at Nov. 16th, 2024</span>'
+    },    
+    
+    // 暗色图片专用样式
+    {
+        image: './assets/images/background/bg7.jpg',
+        chinese_title: '<span style="color: var(--bs-gray-400);">南昌 · 赣江两岸</span>',
+        english_title: '<span style="color: var(--bs-gray-400);">Two Sides of the Ganjiang river</span>',
+        pic_date: '<span style="color: var(--bs-gray-400);">摄于2025年2月5日 Pictured at Feb. 5th, 2025</span>'
+    },    
 ];
+
+// const backgroundData = [
+//     {
+//         image: './assets/images/background/special_day.jpeg',
+//         chinese_title: '<span style="color: var(--bs-gray-400);">你怎么知道我给我对象450抽出了9个金？</span>',
+//         english_title: '<span style="color: var(--bs-gray-400);">How did you know I got 9 golds in 450 pulls for my GF?</span>',
+//         pic_date: '<span style="color: var(--bs-gray-400);">于2025年3月4日  At Mar. 4th, 2025</span>'
+//     },
+// ];
 
 function preloadImages() {
     backgroundData.forEach(item => {
